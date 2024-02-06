5c857ee39656373613112d083c3acdebf676301d
Danny Chen <dannychen@Dannys-Air.lan1>
2024-02-06 11:18:01 -0500

    fix: removed 'r' that led to text extraction issues

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import pdfplumber
 def main():
     # Testing, but when done, make file be entered from cmd line
     pdf_file = open_file("test_q.pdf")
+    extracted_text_arr = get_text(pdf_file)
 
     # Draft funcs
-    # extracted_text_arr = get_text(pdf_file)
     # make_questions(extracted_text)
 
 
 def open_file(f):
     try:
-        file = pdfplumber.open(f, "r")
+        file = pdfplumber.open(f)
     except FileNotFoundError: 
         sys.exit("File not found")
     
